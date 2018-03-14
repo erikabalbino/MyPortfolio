32f6e4e1717727e71720a9a990a5feb2da90506a
Erika Balbino <erk.balbino@gmail.com>
2018-03-13 19:05:01 -0700

    Add style

--- MyPortfolio/db/seeds.rb
+++ MyPortfolio/db/seeds.rb
@@ -5,23 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-5.times.each do
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
+User.destroy_all
 
-  u = User.create(
-    first_name: first_name,
-    last_name: last_name,
-    profile_title: Faker::Company.profession,
-    profile_company: Faker::Company.name,
-    address: Faker::Lorem.paragraph,
-    email: "#{first_name.downcase}.#{last_name.downcase}@example.com",
-    focus: Faker::Company.buzzword,
-    url_facebook: Faker::Internet.url,
-    url_linkedin: Faker::Internet.url,
-    url_instagram: Faker::Internet.url
-  )
-end
+User.create(
+  first_name: "Liz",
+  last_name: "Balbino",
+  profile_title: "Graphic Design Student",
+  profile_company: "The Art Institute of Vancouver",
+  address: "Burnaby, British Columbia, Canada",
+  email: "mecbalbino@gmail.com",
+  focus: "Graphic Designer,Illustration,Digital Art",
+  url_facebook: "http://facebook.com/mebalbino",
+  url_linkedin: "http://linkedin.com/in/mebalbino",
+  url_instagram: "http://instagram.com/mebalbino",
+  image: "https://mir-s3-cdn-cf.behance.net/user/138/a6840c65646341.59346d236302e.jpg"
+)
+
+
+
+# 5.times.each do
+#   first_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
+#
+#   u = User.create(
+#     first_name: first_name,
+#     last_name: last_name,
+#     profile_title: Faker::Company.profession,
+#     profile_company: Faker::Company.name,
+#     address: Faker::Lorem.paragraph,
+#     email: "#{first_name.downcase}.#{last_name.downcase}@example.com",
+#     focus: Faker::Company.buzzword,
+#     url_facebook: Faker::Internet.url,
+#     url_linkedin: Faker::Internet.url,
+#     url_instagram: Faker::Internet.url
+#   )
+# end
 
 users = User.all
 
