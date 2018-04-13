854fc006404c7800a2402a084d9060beec2830fc
Erika Balbino <erk.balbino@gmail.com>
2018-04-13 12:30:41 -0700

    Update gitignore

--- config/initializers/app_keys.rb
+++
@@ -1,5 +0,0 @@
-ENV['AWS_ACCESS_KEY_ID'] = 'AKIAJHJF6BXRNK7R7NSQ'
-ENV['AWS_SECRET_ACCESS_KEY'] = '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'
-
-# ENV['EMAIL_USERNAME'] = 'answerawesome@gmail.com'
-# ENV['EMAIL_PASSWORD'] = 'Sup3r$ecret$$'
