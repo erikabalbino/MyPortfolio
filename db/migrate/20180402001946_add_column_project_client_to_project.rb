ccdd65f3c3e8ef15d05d8baec370c855af98397b
Erika Balbino <erk.balbino@gmail.com>
2018-04-12 19:22:50 -0700

    Fix migration

--- db/migrate/20180402001946_add_column_project_client_to_project.rb
+++ db/migrate/20180402001946_add_column_project_client_to_project.rb
@@ -1,5 +1,6 @@
 class AddColumnProjectClientToProject < ActiveRecord::Migration[5.1]
   def change
     add_column :projects, :project_client, :text
+    add_column :projects, :project_type, :text
   end
 end
