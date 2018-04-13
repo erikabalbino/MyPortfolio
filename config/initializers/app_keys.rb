f1eed5d98a058dd786da0fb3dfb360ad2edb51b5
Erika Balbino <erk.balbino@gmail.com>
2018-04-12 19:55:58 -0700

    Avoid overwriting heroku

--- config/initializers/app_keys.rb
+++ config/initializers/app_keys.rb
@@ -3,5 +3,3 @@ ENV['AWS_SECRET_ACCESS_KEY'] = 'c8+HU3eAOnd33B7yJraFjtAGULwIdmX4se7F0m5a'
 
 # ENV['EMAIL_USERNAME'] = 'answerawesome@gmail.com'
 # ENV['EMAIL_PASSWORD'] = 'Sup3r$ecret$$'
-ENV['EMAIL_USERNAME'] = 'erk.balbino@gmail.com'
-ENV['EMAIL_PASSWORD'] = 'Vitoria01'
