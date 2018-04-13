4ffe4c5a03ae8abda94afc0fce1a92ba0a25c7ef
Erika Balbino <erk.balbino@gmail.com>
2018-04-12 19:32:48 -0700

    Remove domain key

--- config/initializers/setup_active_mailer.rb
+++ config/initializers/setup_active_mailer.rb
@@ -1,7 +1,6 @@
 Rails.configuration.action_mailer.smtp_settings = {
   address: 'smtp.gmail.com',
   port: 587,
-  domain: 'gmail.com',
   user_name: ENV['EMAIL_USERNAME'],
   password: ENV['EMAIL_PASSWORD'],
   authentication: 'plain',
